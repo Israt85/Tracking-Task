878b5acc46e3419f83793f38cffc6b56c8c9262e
Israt85 <isratanjuman3@gmail.com>
2024-03-27 00:34:10 +0600

    added more

--- src/Layout/Card/Card.jsx
+++ src/Layout/Card/Card.jsx
@@ -1,18 +1,48 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import Swal from "sweetalert2";
 
 
 const Card = ({Title,color}) => {
 
     const [tasks, setTasks]= useState([])
     useEffect(() => {
-        fetch('/Task.json')
-            .then(res => res.json()) 
-            .then(data => {
-                console.log(data);
-                setTasks(data);
+        axios.get('http://localhost:3000/tracking')
+            .then(res =>{
+               console.log(res.data)
+               setTasks(res.data)}) 
+            .catch(error => {
+                console.log(error);
+              
             })
     }, [])
     
+    const handleDelete = (user) => {
+      Swal.fire({
+          title: "Are you sure?",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, delete it!"
+      }).then((result) => {
+          if (result.isConfirmed) {
+              axios.delete(`http://localhost:3000/tracking/${user._id}`)
+                  .then(res => {
+                      if (res.data.deletedCount > 0) {
+                          Swal.fire({
+                              title: "Deleted!",
+                              text: "Your file has been deleted.",
+                              icon: "success"
+                          })
+                          
+                      }
+                  })
+          }
+      });
+  }  
     
     return (
         <div className="bg-white h-auto rounded-lg ">
@@ -21,10 +51,98 @@ const Card = ({Title,color}) => {
            </div>
            {
     tasks?.map(task => (
-        task.status.toLowerCase() === Title.toLowerCase() && (
-            <h2 className="p-2 text-black" key={task.id}>
+        task.status?.toLowerCase() === Title?.toLowerCase() && (
+
+
+            <div key={task?.id} className="m-2 h-auto bg-base-200 ">
+  <div className="p-2 text-black">
+ <div className="flex justify-between items-center my-2 border-b-2 border-black">
+ <h2 className="card-title  text-sm">
                 {task.title}
             </h2>
+            <span className="bg-blue-900 px-1 rounded-sm text-white">{task?.priority}</span>
+ </div>
+    <p className="text-xs">{task?.description}</p>
+    <div className="flex my-1 justify-between flex-row">
+        <h2 className="text-sm">{task?.assignee}</h2>
+        <h2 className="bg-blue-900 text-white p-1">
+        <div className="dropdown dropdown-right">
+  <div tabIndex={0}><BsThreeDotsVertical/></div>
+  <ul tabIndex={0} className="dropdown-content text-black z-[1] menu p-2 bg-base-200 rounded-md font-medium w-32">
+   <button className="btn" onClick={()=>document.getElementById('my_modal_4').showModal()}>Edit</button>
+<dialog id="my_modal_4" className="modal">
+ <div className="modal-box rounded-none w-full p-0">
+  <form className="flex px-4 items-center justify-between h-10" method="dialog">
+    <h2 className="font-bold">EDIT TASK</h2>
+      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+    </form>
+    <form className="card-body h-full bg-purple-100">
+        <div className=" ">
+          <label className="label w-24 ">
+            <span className="label-text">Title: </span>
+          </label>
+          <input type="email" className="py-1 rounded-md w-full" required />
+        </div>
+        <div>
+          <label className="label w-18 ">
+            <span className="label-text">Description: </span>
+          </label>
+          <input type="email" className="py-3 rounded-md w-full" required />
+        </div>
+        <div>
+          <label className="label w-24 ">
+            <span className="label-text">Team: </span>
+          </label>
+          <input type="email" className="py-1 rounded-md w-full" required />
+        </div>
+        <div>
+          <label className="label w-24 ">
+            <span className="label-text">Assignees: </span>
+          </label>
+          <input type="email" className="py-1 rounded-md w-full" required />
+        </div>
+      <div className="flex justify-between">
+      <div className=" flex w-full gap-1">
+          <label className="label ">
+            <span className="label-text">Piority:  </span>
+          </label>
+          <select className="w-10 rounded-md" name="" id="">
+            <option value="P0">P0</option>
+            <option value="P1">P1</option>
+            <option value="P2">P2</option>
+          </select>
+          
+        </div>
+        <div className=" flex w-full gap-1">
+          <label className="label ">
+            <span className="label-text">Status:  </span>
+          </label>
+         <input className="border border-black rounded-lg w-20 text-center" type="text" defaultValue='Completed' />
+          
+        </div>
+      </div>
+        
+      </form>
+  </div>
+</dialog>
+
+
+     
+   
+
+
+
+
+    <li><a onClick={()=>handleDelete(task)} >Delete</a></li>
+  </ul>
+</div>
+        </h2>
+    </div>
+    
+    <button className="bg-blue-900 px-2 py-1 text-white rounded-lg">{task.status}</button>
+  </div>
+</div>
+           
         )
     ))
 }
--- src/Layout/TaskLayout/TaskLayout.jsx
+++ src/Layout/TaskLayout/TaskLayout.jsx
@@ -1,59 +1,117 @@
+import axios from "axios";
 import Card from "../Card/Card";
-
+import { useForm } from "react-hook-form"
+import Swal from "sweetalert2";
 
 
 const TaskLayout = () => {
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+  const onSubmit= (data) =>{ 
+    console.log(data)
+  const obj ={
+    title: data.title,
+    description: data.description,
+    team: data.team,
+    assignees: data.assignees,
+    piority: data.piority,
+    status:'Pending'
+  }
+  axios.post('http://localhost:3000/tracking', obj)
+  .then(res=>{
+  console.log(res.data);
+  if(res.data.insertedId){
+    Swal.fire({
+      position: "top-center",
+      icon: "success",
+      title: "Your Task have been saved.",
+      showConfirmButton: false,
+      timer: 1500
+    });
+  }
+  })
+  .catch(error=>{
+  console.log(error);
+  })
+
+  document.getElementById('my_modal_3').close();
+}
     return (
-        <div className="border-2 border-white p-4 rounded-lg h-96 m-6">
-           <div className="flex justify-between my-2">
+        <div className="border-2 border-white p-4 rounded-lg h-auto m-6">
+           <div className="flex flex-col gap-2 lg:flex-row justify-between my-2">
             <div>
-            <p>Filter By :</p>
+            <p className="flex flex-col lg:flex-row gap-2">Filter By :
+            <div>
+            <input className="w-20 ml-2 text-center" type="text" name="" id="" placeholder="Assignee"/>
+            </div>
+              <div>
+              <select name="" id="">
+            <option defaultValue="Piority">Piority</option>
+            <option value="P0">P0</option>
+            <option value="P1">P1</option>
+            <option value="P2">P2</option>
+            </select>
+              </div>
+              <div>
+                <input type="date" name="" id="" />
+              </div>
+            </p>
+           
             </div>
             <div className="bg-blue-900 text-white px-10 py-2 rounded-md">
-            
+{/* Add Task button */}
 <button onClick={()=>document.getElementById('my_modal_3').showModal()}>Add New Task</button>
 </div>
 <dialog id="my_modal_3" className="modal">
-  <div className="modal-box rounded-none w-full p-0">
+  <div className="modal-box rounded-none w-60 lg:w-full p-0">
     <form className="flex px-4 items-center justify-between h-10" method="dialog">
     <h2 className="font-bold">CREATE A TASK</h2>
       <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
     </form>
-    <form className="card-body h-full bg-purple-100">
-        <div className=" flex w-full gap-2 justify-between">
+    {/* Task form */}
+    <form onSubmit={handleSubmit(onSubmit)} className="card-body h-full bg-purple-100">
+        <div className=" flex flex-col lg:flex-row w-full gap-2 justify-between">
           <label className="label w-24 ">
             <span className="label-text">Title: </span>
           </label>
-          <input type="email" className="py-1 rounded-md w-full" required />
+          <input type="text"  {...register("title")} className="py-1 rounded-md w-full" required />
         </div>
-        <div className=" flex w-full gap-2 justify-between">
+        <div className=" flex flex-col lg:flex-row w-full gap-2 justify-between">
           <label className="label w-18 ">
             <span className="label-text">Description: </span>
           </label>
-          <input type="email" className="py-3 rounded-md w-full" required />
+          <input type="text" {...register("description")} className="py-3 rounded-md w-full" required />
         </div>
-        <div className=" flex w-full gap-2 justify-between">
+        <div className=" flex flex-col lg:flex-row w-full gap-2 justify-between">
           <label className="label w-24 ">
             <span className="label-text">Team: </span>
           </label>
-          <input type="email" className="py-1 rounded-md w-full" required />
+          <input type="text"  {...register("team")} className="py-1 rounded-md w-full" required />
         </div>
-        <div className=" flex w-full gap-2 justify-between">
+        <div className=" flex flex-col lg:flex-row w-full gap-2 justify-between">
           <label className="label w-24 ">
             <span className="label-text">Assignees: </span>
           </label>
-          <input type="email" className="py-1 rounded-md w-full" required />
+          <input type="text"  {...register("assignees")} className="py-1 rounded-md w-full" required />
         </div>
         <div className=" flex w-full gap-2">
           <label className="label w-20">
             <span className="label-text">Piority:  </span>
           </label>
-          <select className="w-10 rounded-md" name="" id="">
+          <select {...register("piority")} className="w-10 rounded-md" name="piority" id="">
             <option value="P0">P0</option>
             <option value="P1">P1</option>
             <option value="P2">P2</option>
           </select>
           
+        </div>
+        <div className="max-w-[content] mx-auto">
+        <button className="btn bg-blue-600 p-2 rounded-lg text-white">Submit</button>
         </div>
         
       </form>
@@ -67,9 +125,20 @@ const TaskLayout = () => {
             
             
            </div>
-           <p className="to-blue-950">Sort By :</p>
+           <p className="flex gap-2 to-blue-950">Sort By :
+           
+           <div>
+              <select name="" id="">
+            <option defaultValue="Piority">Piority</option>
+            <option value="P0">P0</option>
+            <option value="P1">P1</option>
+            <option value="P2">P2</option>
+            </select>
+              </div>
+           </p>
+          
 
-           <div className="grid grid-cols-5 text-white mt-4 gap-4">
+           <div className="grid grid-cols-2 lg:grid-cols-5 text-white mt-4 gap-4">
             <Card color={'bg-gray-400'} Title={'Pending'}></Card>
             <Card color={'bg-yellow-600'} Title={'In progress'}/>
             <Card color={'bg-green-500'} Title={'completed'}/>
